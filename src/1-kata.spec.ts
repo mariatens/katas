a36a932f29ecd6dc437b8c0c8ce4d38a1db2aed7
Maria <mtens17@gmail.com>
2022-10-27 12:06:27 +0100

    Rename files

--- src/1-kata.spec.ts
+++
@@ -1,5 +0,0 @@
-import sentenceSmash from './1-kata'
-
-test("sentenceSmash should take an array of words and smash them into a sentence", () => {
-expect(sentenceSmash(["hello", "world"])).toBe("hello world");
-});
--- src/1-kata.ts
+++
@@ -1,8 +0,0 @@
-// Write a function that takes an array of words and smashes them together into a sentence and returns the sentence. 
-
-function sentenceSmash(sentArr: string[]): string {
-    return sentArr.join(' ')
-
-}
-
-export default sentenceSmash
