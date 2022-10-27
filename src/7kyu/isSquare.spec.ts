a89520dd1d2b8451009ae92d0c4ba134fd977f05
Maria <mtens17@gmail.com>
2022-10-27 12:55:42 +0100

    Add version w/o Math methods, not working yet

--- src/7kyu/isSquare.spec.ts
+++ src/7kyu/isSquare.spec.ts
@@ -1,5 +1,5 @@
 import isSquare from './isSquare'
 
 test("isSquare should tell if a number is a squared number", () => {
-    expect(isSquare(25)).toBe(true);
+    expect(isSquare(25)).toEqual(true);
 });
--- src/7kyu/isSquare.ts
+++ src/7kyu/isSquare.ts
@@ -1,14 +1,40 @@
 // https://www.codewars.com/kata/54c27a33fb7da0db0100040e/train/javascript
 
-function isSquare(num: number): boolean {
-    let sq = Math.sqrt(num)
-    if (sq - Math.floor(sq) !== 0){
-        return false
+// without Math methods
+
+function isSquare(num: number): any{
+    if(num < 0){
+        for (let i = num + 1; i < 0; i++){
+            if (num / i === -i){
+                return true
+            }
+            else {
+                return false
+            }
+        }
     }
-    else {
-        return true
+    else if (num > 0){
+        for (let i = num -1; i > 1; i--){
+            if (num / i === i){
+                return true
+            }
+            else {
+                return false
+            }
+        }
     }
+    
 }
+
+// function isSquare(num: number): boolean {
+//     let sq = Math.sqrt(num)
+//     if (sq - Math.floor(sq) !== 0){
+//         return false
+//     }
+//     else {
+//         return true
+//     }
+// }
 
 // simpler solution from codewars
 // function isSquare(n) {
