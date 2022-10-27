472710aea924f939cae0fa69014cf69aad276952
Maria <mtens17@gmail.com>
2022-10-27 11:37:05 +0100

    Complete first kata

---
+++ src/1-kata.ts
@@ -0,0 +1,8 @@
+// Write a function that takes an array of words and smashes them together into a sentence and returns the sentence. 
+
+function sentenceSmash(sentArr: string[]): string {
+    return sentArr.join(' ')
+
+}
+
+export default sentenceSmash
