5fad5cb1f216ef346a994092e354e08a14dd0388
Maria <mtens17@gmail.com>
2022-10-27 12:22:25 +0100

    Fix file path

--- src/8kyu/sentenceSmash.spec.ts
+++ src/8kyu/sentenceSmash.spec.ts
@@ -1,4 +1,4 @@
-import sentenceSmash from './1-kata'
+import sentenceSmash from './sentenceSmash'
 
 test("sentenceSmash should take an array of words and smash them into a sentence", () => {
 expect(sentenceSmash(["hello", "world"])).toBe("hello world");
