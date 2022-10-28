6e104e2782ff1f14d640228a693c029af46442cf
Maria <mtens17@gmail.com>
2022-10-28 14:20:35 +0100

    Different tests

--- src/7kyu/isSquare.spec.ts
+++ src/7kyu/isSquare.spec.ts
@@ -1,5 +1,5 @@
 import isSquare from './isSquare'
 
 test("isSquare should tell if a number is a squared number", () => {
-    expect(isSquare(0)).toBe(true);
+    expect(isSquare(4)).toBe(true);
 });
