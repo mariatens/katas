0ab386bae608cee89a5424b29ea1c5c58a49bdba
Maria <mtens17@gmail.com>
2022-10-31 15:37:58 +0000

    Add simple version of code

---
+++ src/exam/simpler-pairStrings.spec.ts
@@ -0,0 +1,5 @@
+import simpPairStrings from './simpler-pairStrings'
+
+test("pairStrings ", () => {
+    expect(simpPairStrings("abc*def")).toBe("adbecf");
+});
---
+++ src/exam/simpler-pairStrings.ts
@@ -0,0 +1,7 @@
+function simpPairStrings(str: string): string { 
+    let arr = str.split('')
+    let first = arr.slice(0, arr.indexOf("*"))
+    let second = arr.slice(arr.indexOf("*")+1, arr.length)
+    let pairedArray = first.map((i,j)=> i + second[j])
+    return pairedArray.join('')
+}
