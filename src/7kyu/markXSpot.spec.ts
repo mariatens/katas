0614f91a449804aad21da4f05d9c00c2da0fec62
Maria <mtens17@gmail.com>
2022-11-03 14:30:51 +0000

    Function working, need one more condition, jest not working

--- src/7kyu/markXSpot.spec.ts
+++ src/7kyu/markXSpot.spec.ts
@@ -1,5 +1,17 @@
 import markXSpots from './markXSpot'
 
 test("markXSpots given a two dimensional array, return the co-ordinates of x", () => {
-    expect(markXSpots(['o','x'])).toBe(1);
+    expect(markXSpots([
+        ['o', 'o'],
+        ['o', 'o']
+      ])).toBe([]);
+});
+
+test("markXSpots given a two dimensional array, return the co-ordinates of x", () => {
+    expect(markXSpots(
+        [
+            ['x', 'o'],
+            ['o', 'o']
+          ]
+      )).toBe([[0,0]]);
 });
--- src/7kyu/markXSpot.ts
+++ src/7kyu/markXSpot.ts
@@ -1,11 +1,32 @@
-//Given a two dimensional array, return the co-ordinates of x.
-// function markXSpot(nestArr: string[][]): number[]{
-    
-// }
+//https://www.codewars.com/kata/5777fe3f355edbf0a5000d11
 
-// do with simple array first
-function markXSpots(nestArr: string[]): number{
-    return nestArr.indexOf("x")
+//Given a two dimensional array, return the co-ordinates of x.
+// plan
+// go through each subarray, if array includes x, push the index of the subarray and the index of x
+
+// add condition that if more than once, then it's empty
+
+function markXSpot(nestArr: string[][]): number[]{
+    let positions = []
+    for (let [index, subarray] of nestArr.entries()){
+        for (let [subindex, word] of subarray.entries()){
+            if (word === "x"){
+                positions.push(index, subindex)
+            }
+        }
+    }
+    return positions
 }
 
-export default markXSpots
+export default markXSpot
+
+
+// console.log(markXSpot([['1', '2'],['x']]))
+// console.log(markXSpot([
+//     ['o', 'o', 'o', 'o', 'o', 'o', 'o', 'o'],
+//     ['o', 'o', 'o', 'o', 'o', 'o', 'o', 'o'],
+//     ['o', 'o', 'o', 'o', 'o', 'o', 'o', 'o'],
+//     ['o', 'o', 'o', 'o', 'o', 'o', 'o', 'o'],
+//     ['o', 'o', 'o', 'o', 'o', 'o', 'x', 'o'],
+//     ['o', 'o', 'o', 'o', 'o', 'o', 'o', 'o']
+//   ]))
