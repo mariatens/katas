076a6a4f476f74f47d5186bcedea7bdd510ca9f8
Maria <mtens17@gmail.com>
2022-11-04 14:42:19 +0000

    Try again collectByLetter

--- src/problem-solving/collectByLetter.spec.ts
+++ src/problem-solving/collectByLetter.spec.ts
@@ -1,6 +1,6 @@
 import collectByLetter from "./collectByLetter"
 
-let words = ["shall", "i", "compare", "thee", "to", "a", "summer's"]
+let words = ["shall", "i", "compare", "thee", "to", "a", "summers"]
 
 test("", () => {
     expect(collectByLetter(words)).toBe({s: ["shall", "summers"],c: ["compare"],t: ["thee", "to"],i: ["i"],a: ["a"],});
--- src/problem-solving/collectByLetter.ts
+++ src/problem-solving/collectByLetter.ts
@@ -1,18 +1,21 @@
 // https://www.notion.so/weareacademy/Programming-Exercises-Katas-217703f82e9d4b63ba5511396707c6cb?p=f4a1ffb5354541e4b169f373e0894df6&pm=s
-//[index: string]: string[]
+
 
 function collectByLetter(words: string[]): {[index: string]: string[]}{
     let obj: {[index: string]: string[]} = {}
-    for (let pair in obj){
         for (let word of words){
-            if(word[0]===pair){
-                obj[pair].push(word)
+            console.log(word)
+            if(obj[word[0]]){
+                obj[word[0]].push(word)
+            }
+            else{
+                obj[word[0]] = [word]
             }
         }
+        return obj
     }
-    return obj
-    console.log(obj)
-}
+  
+
     
 
 
