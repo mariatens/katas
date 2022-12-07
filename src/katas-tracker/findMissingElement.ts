6a2f563f46086f55e64e5905eebdd317707b9236
Maria <mtens17@gmail.com>
2022-12-07 13:19:50 +0000

    findMissingElement with indexOf and not includes working

--- src/katas-tracker/findMissingElement.ts
+++ src/katas-tracker/findMissingElement.ts
@@ -1,15 +1,26 @@
 
+//two arrays, the one that is in the expected but not in the actual one
 
+import exp from "constants";
 
-export default function findMissingElements(expArr: string[], actArr: string[]): string[]{
-    let tracker = false
-    let missingArr = []
-    for (const expItem of expArr){
-      if (!actArr.includes(expItem)){
-        missingArr.push(expItem)
-      } 
-    }
-    return missingArr
-  }
+// export default function findMissingElements(expArr: string[], actArr: string[]): string[]{
+//     let missingArr = []
+//     for (const expItem of expArr){
+//       if (!actArr.includes(expItem)){
+//         missingArr.push(expItem)
+//       } 
+//     }
+//     return missingArr
+//   }
 
   // NEED TO DO IT WITHOUT INCLUDES!!
+  export default function findMissingElements(expArr: string[], actArr: string[]){
+    const array = []
+    for (let item of expArr){
+      if (actArr.indexOf(item) === -1){ //if not present in the actual array
+        array.push(item)
+      }
+    }
+  return array
+}
+       
