af78b32343f8adcdcde6b34d6288183bd1aa9f28
Maria <mtens17@gmail.com>
2022-12-26 12:21:45 +0000

    Repeat function working

---
+++ src/problem-solving/higher-order-functions/repeat.ts
@@ -0,0 +1,11 @@
+function repeat(n: number, callbackFn: Function): undefined{
+    for (let i = 0; i <= n; i++){
+        callbackFn()
+    }
+    return undefined
+}
+function hello(){
+    return "hello"
+}
+
+console.log(repeat(2, hello))
