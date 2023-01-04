985bd234a29b2ba629426a36551dbf3b7f27825a
Maria <mtens17@gmail.com>
2023-01-04 10:06:58 +0000

    Format and optimised

--- src/problem-solving/sum-nested-arrays.ts
+++ src/problem-solving/sum-nested-arrays.ts
@@ -1,25 +1,20 @@
 // https://www.notion.so/weareacademy/Sum-nested-arrays-74e3f58890f642439850d7536c1a17b5
 
-type NumberOrNested = number | NumberOrNested[]
+type NumberOrNested = number | NumberOrNested[];
 
 // non recursive strategy with stack
 
-function sumNested(nestArr : NumberOrNested[]): number{
-  const workStack = []
-  let sum = 0
-  for (let element of nestArr){
-    workStack.push(element)
-    while (workStack.length >= 1) {
-        let currentNumber = workStack.pop() as NumberOrNested
-        if (typeof currentNumber === "number"){
-            sum += currentNumber
-        }
-        else{
-            for (let subelement of currentNumber){
-                workStack.push(subelement)
-            }
-        }    
+function sumNested(nestArr: NumberOrNested[]): number {
+  const workStack: NumberOrNested[] = [];
+  let sum = 0;
+  workStack.push(nestArr);
+  while (workStack.length >= 1) {
+    let current = workStack.pop()!;
+    if (typeof current === "number") {
+      sum += current;
+    } else {
+      workStack.push(...current);
     }
   }
-    return sum
+  return sum;
 }
